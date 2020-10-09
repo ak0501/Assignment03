f293d7598afccb644ad74463d88c8b7ea854e9ff
khab0501 <48302535+khab0501@users.noreply.github.com>
2020-10-09 15:51:10 -0500

    Added if else satement

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-function generatePassword(length, use_upper, use_lower, use_number, use_sp_chars) {
+function generatePassword(
+  length,
+  use_upper,
+  use_lower,
+  use_number,
+  use_sp_chars
+) {
   if (length < 8 || length > 128) {
     alert("password must be between 8 and 128 char long");
   } else {
     if (use_upper || use_lower || use_number || use_sp_chars) {
-      var uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-      var numbers = '1234567890';
+      var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+      var numbers = "1234567890";
       var specialChars = '!@#$%^&*()_+~`|}{[]\\:;?><,./-="';
-      var charactersUsed = '';
-      var password = '';
+      var charactersUsed = "";
+      var password = "";
       if (use_upper) charactersUsed += uppercaseLetters;
       if (use_lower) charactersUsed += lowercaseChars;
       if (use_number) charactersUsed += numbers;
       if (use_sp_chars) charactersUsed += specialChars;
       for (var i = 0; i < length; i++) {
-        password += charactersUsed.charAt(Math.floor(Math.random() * charactersUsed.length));
+        password += charactersUsed.charAt(
+          Math.floor(Math.random() * charactersUsed.length)
+        );
       }
       return password;
     } else {
@@ -24,21 +32,34 @@ function generatePassword(length, use_upper, use_lower, use_number, use_sp_chars
 }
 //Call the function
 function writePassword(length, use_upper, use_lower, use_number, use_sp_chars) {
-// store the return variable from the function
-  var password = generatePassword(length, use_upper, use_lower, use_number, use_sp_chars);
+  // store the return variable from the function
+  var password = generatePassword(
+    length,
+    use_upper,
+    use_lower,
+    use_number,
+    use_sp_chars
+  );
   // Paste the return value to the text box in password field
-  document.getElementById('password').innerHTML = password;
+  document.getElementById("password").innerHTML = password;
 }
 // Press the generate password button to ask the length of the password
 document.getElementById("generate").addEventListener("click", function () {
   var length = prompt("enter the password length");
-  //prompt if they want upper case letters
-  var use_upper = confirm("Do you want uppercase letters?");
-  //prompt if they want lowercase letters
-  var use_lower = confirm("lowercase letter?");
-  //prompt if they want numeric values
-  var use_number = confirm("Do you want numbers?");
-  //prompt if they want special characters
-  var use_sp_chars = confirm("Do you want special characters?");
-  writePassword(length, use_upper, use_lower, use_number, use_sp_chars);
+
+  if (length === null) {
+    return;
+  } else {
+
+    
+    //prompt if they want upper case letters
+    var use_upper = confirm("Do you want uppercase letters?");
+    //prompt if they want lowercase letters
+    var use_lower = confirm("lowercase letter?");
+    //prompt if they want numeric values
+    var use_number = confirm("Do you want numbers?");
+    //prompt if they want special characters
+    var use_sp_chars = confirm("Do you want special characters?");
+    writePassword(length, use_upper, use_lower, use_number, use_sp_chars);
+  }
 });
